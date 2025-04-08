1b50e4edf37c3112895be2385028c1557d4f75c7
Himanshu <himanshu290304@gmail.com>
2025-04-09 00:46:46 +0530

    did something

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
 import streamlit as st
 import pandas as pd
-import Preprocessor,helper
+import Preprocessor
+import helper
 
 # Load datasets
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
 
-df = Preprocessor.preprocess(df,region_df)
+df = Preprocessor.preprocess(df, region_df)
 
 st.sidebar.title("Olympics Analysis")
 user_menu = st.sidebar.radio(
     'Select an option',
-    ('Medal Tally','Overall Analysis', 'Country-Wise Analysis', 'Athlete Wise Analysis')
+    ('Medal Tally', 'Overall Analysis',
+     'Country-Wise Analysis', 'Athlete Wise Analysis')
 
 )
 
-st.dataframe(df)
+# st.dataframe(df)
 
 if user_menu == "Medal Tally":
     st.sidebar.header("Medal Tally")
-    years,country = helper.country_year_list(df)
+    years, country = helper.country_year_list(df)
 
-    selected_year = st.sidebar.selectbox("Select the Year",years)
-    selected_country = st.sidebar.selectbox("Select the country",country)
+    selected_year = st.sidebar.selectbox("Select the Year", years)
+    selected_country = st.sidebar.selectbox("Select the country", country)
 
-    medal_tally = helper.medal_tally(df)
-    st.dataframe(medal_tally)
+    medal_tally = helper.fetch_medal_tally(df, selected_year, selected_country)
+
+    if selected_country == "Overall" and selected_year == "Overall":
+        st.title("Overall Performance Across All Years and Countries")
+    elif selected_country == "Overall" and selected_year != "Overall":
+        st.title(f"Performance Across All Countries in {selected_year}")
+    elif selected_country != "Overall" and selected_year == "Overall":
+        st.title(f"Performance of {selected_country} Across All Years")
+    else:
+        st.title(f"Performance of {selected_country} in {selected_year}")
+
+    st.table(medal_tally)
--- helper.py
+++ helper.py
@@ -16,8 +16,7 @@ def medal_tally(df):
     return medal_tally
 
 def country_year_list(df):
-    years = df['Year'].unique().tolist()
-    years.sort
+    years = sorted(df['Year'].unique().tolist())
     years.insert(0,'Overall')
 
     country = np.unique(df['region'].dropna().values.tolist())
@@ -26,3 +25,29 @@ def country_year_list(df):
 
     return years,country
 
+def fetch_medal_tally(df, year, country):
+    medal_tally = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
+    flag = 0
+    if year == 'Overall' and country == 'Overall': 
+        temp_df = medal_tally
+    if year == 'Overall' and country != 'Overall':
+        flag = 1
+        temp_df = medal_tally[medal_tally['region'] == country]
+    if year != 'Overall' and country == 'Overall':
+        temp_df = medal_tally[medal_tally['Year'] == int(year)]
+    if year != 'Overall' and country != 'Overall':
+        temp_df = medal_tally[(medal_tally['Year'] == int(year)) & (medal_tally['region'] == country)]
+
+    if flag == 1: 
+        x = temp_df.groupby('Year').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Year', ascending=True).reset_index()
+    else:
+        x = temp_df.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending=False).reset_index()
+
+    x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
+
+    x['Gold'] = x['Gold'].astype('int')
+    x['Silver'] = x['Silver'].astype('int')
+    x['total'] = x['total'].astype('int')
+    x['Bronze'] = x['Bronze'].astype('int')
+
+    return x
